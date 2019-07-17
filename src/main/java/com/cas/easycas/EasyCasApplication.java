733485b4dc93218bd5718dc11beff88180d3e706
Zogoo <chtsogbadrakh@gmail.com>
2019-07-17 20:11:03 +0900

    Gradle build

---
+++ src/main/java/com/cas/easycas/EasyCasApplication.java
@@ -0,0 +1,13 @@
+package com.cas.easycas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCasApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,4 @@
+create table USER (
+    ID int not null,
+    EMAIL varchar(100) not null,
+)
